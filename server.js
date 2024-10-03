2181b122ce07058554d190eaee9db3cf6b859852
rjparsana <160236867+rjparsana@users.noreply.github.com>
2024-10-04 05:15:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const http = require("http");
+const route = require("./routes");
+const bodyParser = require("body-parser");
+
+const express = require("express");
+const connectDB = require("./db/dbConnect");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use("/v1", route);
+
+// app.get('/', (req, res) => {
+//   res.send('Blog API is running');
+// });
+
+
+connectDB();
+
+http.createServer(app).listen(process.env.PORT, () => {
+  console.log("server started successs on port", process.env.PORT);
+});
